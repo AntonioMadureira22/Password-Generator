5091ac5146ceb4314c21541ab0b32e011f427f53
Antonio Madureira <antoniomadureira2222@gmail.com>
2021-08-11 22:48:43 -0400

    JS starter

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const numbers="0123456789";
+const symbols="!@#$%^&*()_=+~/?|";
 
+alert("Welcome to the Password Generator. Where password security is our #1 priorty! Answer the prompts to have your private and exclusive password generated:)");
+var length = prompt("How many characters would you your password to be? It can be NO LESS than 8 and NO MORE then 128.");
+if (!length) {
+  alert("Choose a Vaule!");
+}
+
+else if ( length < 8 || length > 128) {
+  length = prompt("You must choose between 8 and 128 characters.");
+}
+
+confirm("Do You Want UPPERCASE letters?");
+if (true) { 
+
+} else {
+
+}
+confirm("Do You Want LOWERCASE letters?");
+confirm("Do You Want NUMBERS?");
+confirm("Do You Want SYMBOLS?");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
