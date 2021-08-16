fb3a434c6a47fd768dce3db54c135539d451108e
Antonio Madureira <antoniomadureira2222@gmail.com>
2021-08-15 21:42:32 -0400

    Update readme and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,20 @@
 // Assignment code here
-var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase="abcdefghijklmnopqrstuvwxyz";
+var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase ="abcdefghijklmnopqrstuvwxyz";
 var numbers="0123456789";
 var symbols="!@#$%^&*(/)_=+~/?|";
 var length;
 // variable for password choice selector
-var choices;
+var choices= "upperCase, lowerCase, numbers, symbols";
 
 alert("Welcome to the Password Generator. Where password security is our #1 priorty! Answer the prompts to have your private and exclusive password generated. Click the 'Generate Password' button to begin:)");
 
 function generatePassword() {
-  
-  
   length = parseInt (prompt("How many characters would you your password to be? It can be NO LESS than 8 and NO MORE then 128."));
   //value asked for
   if (!length) {
     alert("Charecter Amount Needed!")
-  } else if (length <8 || length > 128) {
+  } else if (length < 8 || length > 128) {
     //needs validation
     length = parseInt(prompt("Password length needs to be between 8 and 128"));
   } else {
@@ -24,58 +22,68 @@ function generatePassword() {
     lowerCase = confirm("Do You Want LOWERCASE letters?");
     numbers = confirm("Do You Want NUMBERS?");
     symbols = confirm("Do You Want SYMBOLS?");
+    //randomization 
+  for (var i = 0 ; i < i++;) {
+    var choices = choices [Math.floor(Math.random() * choices.length)];
+    password.push(choices);
+  
+    }
   }
+return length;
 };
-//If no criteria selected
-if (!upperCase && !lowerCase && !numbers && !symbols){
-  choices = alert("Values need to be selected!");
-}
-//When all of 4 variables are selected
-else if ( upperCase && lowerCase && numbers && symbols){
-  let choices = upperCase + lowerCase + numbers+ symbols;
-}
-//When all of 3 variables are selected
-else if (upperCase && lowerCase && numbers){
-  let choices = upperCase + lowerCase + numbers;
-}
-else if (upperCase && lowerCase && symbols){
-  let choices = upperCase + lowerCase + symbols;
-}
-else if (lowerCase && numbers && symbols){
-  choices = lowerCase + numbers + symbols;
-}
-//When all of 2 variables are selected
-else if (upperCase && lowerCase){
-  let choices = upperCase + lowerCase;
-}
-else if (upperCase && numbers){
-  let choices = upperCase + numbers;
-}
-else if (upperCase && symbols){
-  let choices = upperCase + symbols;
-}
-else if (lowerCase && numbers){
- let choices = lowerCase + numbers;
-}
-else if (lowerCase && symbols){
-  let choices = lowerCase + symbols;
-}
-else if (numbers && symbols){
-  let choices = numbers + symbols;
-}
-//when 1 variable is selcted
-else if (upperCase){
-  let choices = upperCase;
-}
-else if (lowerCase){
-  let choices = lowerCase;
-}
-else if (numbers){
-  let choices = numbers;
-}
-else if (symbols){
-  let choices = symbols;
-}
+
+  
+
+  //If no criteria selected
+  if (!upperCase && !lowerCase && !numbers && !symbols){
+    choices = alert("Values need to be selected!");
+  }
+  //When all of 4 variables are selected
+  else if ( upperCase && lowerCase && numbers && symbols){
+   choices = upperCase + lowerCase + numbers+ symbols;
+  }
+  //When all of 3 variables are selected
+  else if (upperCase && lowerCase && numbers){
+    choices = upperCase + lowerCase + numbers;
+  }
+  else if (upperCase && lowerCase && symbols){
+    choices = upperCase + lowerCase + symbols;
+  }
+  else if (lowerCase && numbers && symbols){
+    choices = lowerCase + numbers + symbols;
+  }
+  //When all of 2 variables are selected
+  else if (upperCase && lowerCase){
+    choices = upperCase + lowerCase;
+  }
+  else if (upperCase && numbers){
+    choices = upperCase + numbers;
+  }
+  else if (upperCase && symbols){
+    choices = upperCase + symbols;
+  }
+  else if (lowerCase && numbers){
+   choices = lowerCase + numbers;
+  }
+  else if (lowerCase && symbols){
+     choices = lowerCase + symbols;
+  }
+  else if (numbers && symbols){
+    choices = numbers + symbols;
+  }
+  //when 1 variable is selcted
+  else if (upperCase){
+     choices = upperCase;
+  }
+  else if (lowerCase){
+     choices = lowerCase;
+  }
+  else if (numbers){
+    choices = numbers;
+  }
+  else if (symbols){
+     choices = symbols;
+  }
 
 
 // Get references to the #generate element
@@ -89,11 +97,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//randomization 
-  for (var i = 0; i < choices; i++){
-  var writePassword = choices [Math.floor(Math.random() * choices.length)];
-  password.push(writePassword);
-  }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Password Generator Starter Code
+Welcome to the Password Generator! Where you're problems are solved for password security. I was given the task to have the password generator be aceesible and functional. The intent is to have the generator display random numbers, letters, and symbols with a length no less than 8 and no greater than 128. 
 
+##SnapShots
+![Screenshot (30)](https://user-images.githubusercontent.com/86333093/129500127-7a86e751-d734-46e0-a9c3-807aebae80a0.png)
+![Screenshot (31)](https://user-images.githubusercontent.com/86333093/129500207-6f44dbfd-3435-42c2-8919-a26072f8e305.png)
+![Screenshot (32)](https://user-images.githubusercontent.com/86333093/129500227-f3e8e6a0-16e7-4ad3-b189-8321b753e85d.png)
+![Screenshot (33)](https://user-images.githubusercontent.com/86333093/129500259-d7044b9f-5d6b-47ab-b748-f2a61f4ff04b.png)
+![Screenshot (34)](https://user-images.githubusercontent.com/86333093/129500337-bdd27c0b-a3e9-4953-ad21-4d22dbe277f4.png)
+![Screenshot (35)](https://user-images.githubusercontent.com/86333093/129500353-68681b83-445a-49fd-adb7-554bcb492536.png)
+![Screenshot (36)](https://user-images.githubusercontent.com/86333093/129500368-0b5b654a-eef4-468a-8306-8d0231d3a687.png)
+![Screenshot (37)](https://user-images.githubusercontent.com/86333093/129500382-c78d8cb8-7f6d-4001-b48d-d14523405877.png)
+
+Thank you for taking the time to view my project. I appreciate it. 
